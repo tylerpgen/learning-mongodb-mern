a908c932578e55956e38e8cecd9359c745634d16
Tyler Gener <tylergen33@gmail.com>
2023-06-11 14:59:31 -0700

    created the table that displays the records

---
+++ client/src/components/RecordList.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const RecordList = () => {
+  // This following section will display the table with the records of individuals.
+  return (
+    <div>
+      <h3>Record List</h3>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Level</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{recordList()}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default RecordList;
