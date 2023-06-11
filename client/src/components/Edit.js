c1943aec0e1c85d1bb164a17b22910f1727b82c2
Tyler Gener <tylergen33@gmail.com>
2023-06-11 14:53:01 -0700

    created form in edit.js

--- client/src/components/Edit.js
+++ client/src/components/Edit.js
@@ -61,9 +61,81 @@ const Edit = () => {
     navigate("/");
   };
 
+  //Form for user edits
   return (
     <div>
-      <div className=""></div>
+      <h3>Update Record</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={form.name}
+            onChange={(e) => updateForm({ name: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="position">Position: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="position"
+            value={form.position}
+            onChange={(e) => updateForm({ position: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="positionOptions"
+              id="positionIntern"
+              value="Intern"
+              checked={form.level === "Intern"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="positionIntern" className="form-check-label">
+              Intern
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="positionOptions"
+              id="positionJunior"
+              value="Junior"
+              checked={form.level === "Junior"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="positionJunior" className="form-check-label">
+              Junior
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="positionOptions"
+              id="positionSenior"
+              value="Senior"
+              checked={form.level === "Senior"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="positionSenior" className="form-check-label">
+              Senior
+            </label>
+          </div>
+        </div>
+        <br />
+
+        <div className="form-group">
+          <input type="submit" value="Update Record" className="btn btn-primary" />
+        </div>
+      </form>
     </div>
   );
 };
