fee3e1d24318edd4fe59570103e0afba6a487eb8
Tyler Gener <tylergen33@gmail.com>
2023-06-13 11:28:37 -0700

    added routes and components to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<RecordList />} />
+          <Route exact path="/edit/:id" element={<Edit />} />
+          <Route exact path="/create" element={<Create />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
